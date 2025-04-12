6fd0c2f0d18deebf0cec8d4ee6d51c832e17dceb
jingyugao <1121087373@qq.com>
2025-04-12 15:49:28 +0800

    rm unused file

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
   <div id="app">
     <h1>Server Manager</h1>
-    <ServerManager 
-      @server-added="handleServerAdded"
-      @server-removed="handleServerRemoved"
-      @server-connected="handleServerConnected"
-      @server-disconnected="handleServerDisconnected"
-      @server-selected="handleServerSelected"
-    />
+    <ServerManager />
   </div>
 </template>
 
@@ -18,23 +12,6 @@ export default {
   name: 'App',
   components: {
     ServerManager
-  },
-  methods: {
-    handleServerAdded(server) {
-      console.log('Server added:', server)
-    },
-    handleServerRemoved(server) {
-      console.log('Server removed:', server)
-    },
-    handleServerConnected(server) {
-      console.log('Server connected:', server)
-    },
-    handleServerDisconnected(server) {
-      console.log('Server disconnected:', server)
-    },
-    handleServerSelected(server) {
-      console.log('Server selected:', server)
-    }
   }
 }
 </script>
